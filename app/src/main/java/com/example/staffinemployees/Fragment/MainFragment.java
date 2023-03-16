76da3eba00fda9a915437bfd0b18d270fd032de2
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-03-16 16:04:37 +0530

    event recyclerview

--- app/src/main/java/com/example/staffinemployees/Fragment/MainFragment.java
+++ app/src/main/java/com/example/staffinemployees/Fragment/MainFragment.java
@@ -3,12 +3,16 @@ package com.example.staffinemployees.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.staffinemployees.Adapters.EventAdapter;
+import com.example.staffinemployees.Adapters.HomeEventsAdapter;
 import com.example.staffinemployees.R;
 import com.example.staffinemployees.databinding.FragmentMainBinding;
 
@@ -21,23 +25,26 @@ public class MainFragment extends Fragment {
 
     FragmentMainBinding binding;
 
-    int year,month,day,minute,second,hour;
+    int minute, second, hour;
 
-        @Override
-        public View onCreateView (LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState){
+    RecyclerView.LayoutManager layoutManagerH;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         binding = FragmentMainBinding.inflate(inflater, container, false);
 
+        binding.recyclerViewMonthEvents.setAdapter(new HomeEventsAdapter(getContext()));
+        layoutManagerH=new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false);
+        binding.recyclerViewMonthEvents.setLayoutManager(layoutManagerH);
 
-            setDigitalClock();
 
 
+        setDigitalClock();
         return binding.getRoot();
-
-
     }
 
-        private void setDigitalClock () {
+    private void setDigitalClock() {
         final Handler hander = new Handler();
         new Thread(new Runnable() {
             @Override
@@ -56,7 +63,7 @@ public class MainFragment extends Fragment {
                         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
                             now = LocalDateTime.now();
                             hour = now.getHour();
-                            minute  = now.getMinute();
+                            minute = now.getMinute();
                             second = now.getSecond();
                         }
                         binding.hourTv.setText(String.format("%02d", hour));
@@ -70,4 +77,4 @@ public class MainFragment extends Fragment {
         }).start();
     }
 
-    }
+}
